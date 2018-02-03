990f5047244816bd9cff40622fc2c0873fe7c2e3
Masayoshi Kuroda <kuroda.masayoshi@gmail.com>
2018-02-03 23:29:28 +0900

    add command option

--- remo.py
+++ remo.py
@@ -4,12 +4,21 @@
 import sys
 import os
 import json
-import urllib2
+from urllib2 import Request, urlopen, URLError, HTTPError
+from argparse import ArgumentParser
 
 BASE_URL = "https://api.nature.global"
 BASE_PATH       = os.path.dirname(os.path.abspath(__file__))
 TOKEN_FILE      = os.path.join(BASE_PATH, 'token.txt')
 APPLIANCES_FILE = os.path.join(BASE_PATH, 'appliances.json')
+
+usage = 'Usage: python {} command [--dev_no dev_no] [--nickname nickname] [--name name]'.format(__file__)
+argparser = ArgumentParser(usage=usage)
+argparser.add_argument('command', type=str, help='[get_temp|get_appliances|send_signal]')
+argparser.add_argument('-d', '--dev_no', type=int, dest='dev_no', help='device number for reading temperature')
+argparser.add_argument('-a', '--nickname', type=str, dest='nickname', help='appliance name')
+argparser.add_argument('-n', '--name', type=str, dest='name', help='signal name')
+args = argparser.parse_args()
 
 def get_token():
     f = open(TOKEN_FILE, 'r')
@@ -29,21 +38,38 @@ def get_signal_id(nickname, name):
     f.close()
     return None
 
-if len(sys.argv) <= 2:
-    print 'Usage: python remo.py appliance_name, signal_name'
+url = BASE_URL
+data = None
+if args.command =='get_temp':
+    url += "/1/devices"
+elif args.command == 'get_appliances':
+    url += "/1/appliances"
+elif args.command == 'send_signal':
+    nickname = args.nickname.decode('utf-8')
+    name = args.name.decode('utf-8')
+    signal_id = get_signal_id(nickname, name)
+    url += "/1/signals/" + signal_id + "/send"
+    data = "\r\n"
+else:
+    print "Command error: ", args.command
     sys.exit(1)
-
-nickname = sys.argv[1].decode('utf-8')
-name = sys.argv[2].decode('utf-8')
+# print url
 
 token = get_token()
-signal_id = get_signal_id(nickname, name)
-
-url = BASE_URL + "/1/signals/" + signal_id + "/send"
 headers = { "Authorization" : "Bearer " + token }
-req = urllib2.Request(url, data="\r\n", headers=headers)
+req = Request(url, data=data, headers=headers)
 try:
-    res = urllib2.urlopen(req)
+    res = urlopen(req)
 except HTTPError, e:
-    print e.getcode()
-print res.getcode()
+    print 'Error code: ', e.getcode()
+    sys.exit(1)
+except URLError as e:
+    print 'Reason: ', e.reason
+    sys.exit(1)
+
+body = res.read()
+if args.command == 'get_temp':
+    body = json.loads(body)[args.dev_no]['newest_events']
+    body = json.dumps(body)
+
+print body
